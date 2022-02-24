7a7dc9001371f53810c7bcbb8836935c1c22980a
Alex Piechowski <alex@piechowski.io>
2022-02-24 03:48:34 -0600

    Add Order#delete

--- src/shopify/resources/order.cr
+++ src/shopify/resources/order.cr
@@ -4,8 +4,11 @@ require "../../shopify"
 class Shopify::Order < Shopify::Resource
   indexable
   countable
+  deletable
 
   def self.uri(domain : String, path : String = "") : URI
     URI.parse "https://#{domain}/admin/api/2022-01/orders#{path}.json"
   end
+
+  property id : Int64
 end
