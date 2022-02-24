505f0c519fa71e5de9de3f486a0532ab6c5d88dd
Alex Piechowski <alex@piechowski.io>
2022-02-24 05:06:14 -0600

    Finish off the rest of order

--- src/shopify/resources/order.cr
+++ src/shopify/resources/order.cr
@@ -14,4 +14,73 @@ class Shopify::Order < Shopify::Resource
   end
 
   property id : Int64
+
+  # Under the covers, this just runs:
+  # ```plaintext
+  # POST
+  # /admin/api/2022-01/orders/{id}/cancel.json
+  # ```
+  def cancel : self
+    JSON::PullParser.new(
+      HTTP::Client.post(
+        self.class.uri(store.shop, "/#{id}/cancel"),
+        HTTP::Headers{
+          "X-Shopify-Access-Token" => store.access_token,
+          "Content-Type"           => "application/json",
+        },
+        "{}"
+      ).body
+    ).try do |pull|
+      pull.read_begin_object
+      pull.read_object_key
+
+      self.class.from_json(pull.read_raw).tap &.store=(store)
+    end
+  end
+
+  # Under the covers, this just runs:
+  # ```plaintext
+  # POST
+  # /admin/api/2022-01/orders/{id}/close.json
+  # ```
+  def close : self
+    JSON::PullParser.new(
+      HTTP::Client.post(
+        self.class.uri(store.shop, "/#{id}/close"),
+        HTTP::Headers{
+          "X-Shopify-Access-Token" => store.access_token,
+          "Content-Type"           => "application/json",
+        },
+        "{}"
+      ).body
+    ).try do |pull|
+      pull.read_begin_object
+      pull.read_object_key
+
+      self.class.from_json(pull.read_raw).tap &.store=(store)
+    end
+  end
+
+  # Under the covers, this just runs:
+  # ```plaintext
+  # POST
+  # /admin/api/2022-01/orders/{id}/open.json
+  # ```
+  def open : self
+    JSON::PullParser.new(
+      HTTP::Client.post(
+        self.class.uri(store.shop, "/#{id}/open"),
+        HTTP::Headers{
+          "X-Shopify-Access-Token" => store.access_token,
+          "Content-Type"           => "application/json",
+        },
+        "{}"
+      ).body
+    ).try do |pull|
+      pull.read_begin_object
+      pull.read_object_key
+
+      self.class.from_json(pull.read_raw).tap &.store=(store)
+    end
+  end
 end
